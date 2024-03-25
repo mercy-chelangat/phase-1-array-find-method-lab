4f3f30d03fb3695129ca620ff9013a2a4f725926
mercy-chelangat <chelangatmercy194@gmail.com>
2024-03-26 00:23:11 +0300

    complete assignments

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+// code your solution h
+function superbowlWin(array) {
+    const win = array.find(obj => obj.result === "W");
+    return win ? win.year : undefined;
+}
+
+const games = [
+    { year: 2019, result: "L" },
+    { year: 2020, result: "W" },
+    { year: 2021, result: "L" }
+];
+
+console.log(superbowlWin(games)); 
+
+
